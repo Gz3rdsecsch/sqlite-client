fbe057b042e041390e07a28c5122f03f919b7057
Gz3rdsecsch <108299192+Gz3rdsecsch@users.noreply.github.com>
2022-07-02 14:56:48 -0400

    Add files via upload

---
+++ images/__init__.py
@@ -0,0 +1,7 @@
+from os import path
+
+IMAGE_DIRECTORY = path.dirname(__file__)
+
+LOGO_16 = path.join(IMAGE_DIRECTORY, 'sql_logo-16x10.png')
+LOGO_32 = path.join(IMAGE_DIRECTORY, 'sql_logo-32x20.png')
+LOGO_64 = path.join(IMAGE_DIRECTORY, 'sql_logo-64x40.png')
